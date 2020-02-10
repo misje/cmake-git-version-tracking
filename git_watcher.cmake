3e9e6e153daa0861cd0a7d44e85e781fdfd3d0d9
Andreas Misje <amisje@gmail.com>
2020-02-10 17:02:53 +0100

    Completely rewrite script for version extration from tags
    The added Perl helper script does all the heavy regex.

--- git_watcher.cmake
+++ git_watcher.cmake
@@ -1,9 +1,8 @@
 # git_watcher.cmake
-# https://raw.githubusercontent.com/andrew-hardin/cmake-git-version-tracking/master/git_watcher.cmake
+# https://raw.githubusercontent.com/misje/cmake-git-version-tracking/master/git_watcher.cmake
 #
 # Released under the MIT License.
-# https://raw.githubusercontent.com/andrew-hardin/cmake-git-version-tracking/master/LICENSE
-
+# https://raw.githubusercontent.com/misje/cmake-git-version-tracking/master/LICENSE
 
 # This file defines a target that monitors the state of a git repo.
 # If the state changes (e.g. a commit is made), then a file gets reconfigured.
@@ -37,11 +36,6 @@
 #       2. Build time (called via CMake -P).
 #     The first invocation is what registers the script to
 #     be executed at build time.
-#
-# MODIFICATIONS
-#   You may wish to track other git properties like when the last
-#   commit was made. There are two sections you need to modify,
-#   and they're tagged with a ">>>" header.
 
 # Short hand for converting paths to absolute.
 macro(PATH_TO_ABSOLUTE var_name)
@@ -76,163 +70,54 @@ if(NOT DEFINED GIT_EXECUTABLE)
 endif()
 CHECK_REQUIRED_VARIABLE(GIT_EXECUTABLE)
 
-
 set(_state_variable_names
-    GIT_RETRIEVED_STATE
-    GIT_HEAD_SHA1
-    GIT_IS_DIRTY
-    GIT_AUTHOR_NAME
-    GIT_AUTHOR_EMAIL
-    GIT_COMMIT_DATE_ISO8601
-    GIT_COMMIT_SUBJECT
-    GIT_COMMIT_BODY
-    # >>>
-    # 1. Add the name of the additional git variable you're interested in monitoring
-    #    to this list.
+    # Full semantic version (or empty):
+    GIT_TAG_VERSION_FULL
+    # Full semantic version + "extra" (but not deb revision) (or empty):
+    GIT_TAG_VERSION_FULL_EXTRA
+    # Major version number (or -1):
+    GIT_TAG_VERSION_MAJOR
+    # Minor version number (or -1):
+    GIT_TAG_VERSION_MINOR
+    # Patch version number (or -1):
+    GIT_TAG_VERSION_PATCH
+    # Any extra text between version and deb release number (or empty):
+    GIT_TAG_VERSION_EXTRA
+    # deb release number (or -1):
+    GIT_TAG_VERSION_REVISION
+    # Number of commits since last tag (or -1):
+    GIT_TAG_VERSION_COMMITS
+    # Commit SHA (if not a tag, otherwise empty):
+    GIT_TAG_VERSION_SHA
+    # Whether the working tree is dirty (0 or 1):
+    GIT_TAG_VERSION_DIRTY
+    # If a clean tag, full semantic version, otherwise commit SHA:
+    GIT_TAG_VERSION_ANY
 )
-
-
-
-# Macro: RunGitCommand
-# Description: short-hand macro for calling a git function. Outputs are the
-#              "exit_code" and "output" variables.
-macro(RunGitCommand)
-    execute_process(COMMAND
-        "${GIT_EXECUTABLE}" ${ARGV}
-        WORKING_DIRECTORY "${_working_dir}"
-        RESULT_VARIABLE exit_code
-        OUTPUT_VARIABLE output
-        ERROR_QUIET
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-    if(NOT exit_code EQUAL 0)
-        set(ENV{GIT_RETRIEVED_STATE} "false")
-    endif()
-endmacro()
-
-
 
 # Function: GetGitState
 # Description: gets the current state of the git repo.
 # Args:
 #   _working_dir (in)  string; the directory from which git commands will be executed.
 function(GetGitState _working_dir)
-
-    # This is an error code that'll be set to FALSE if the
-    # RunGitCommand ever returns a non-zero exit code.
-    set(ENV{GIT_RETRIEVED_STATE} "true")
-
-    # Get whether or not the working tree is dirty.
-    RunGitCommand(status --porcelain)
-    if(NOT exit_code EQUAL 0)
-        set(ENV{GIT_IS_DIRTY} "false")
-    else()
-        if(NOT "${output}" STREQUAL "")
-            set(ENV{GIT_IS_DIRTY} "true")
-        else()
-            set(ENV{GIT_IS_DIRTY} "false")
-        endif()
-    endif()
-
-    # There's a long list of attributes grabbed from git show.
-    set(object HEAD)
-    RunGitCommand(show -s "--format=%H" ${object})
-    if(exit_code EQUAL 0)
-        set(ENV{GIT_HEAD_SHA1} ${output})
-    endif()
-
-    RunGitCommand(show -s "--format=%an" ${object})
-    if(exit_code EQUAL 0)
-        set(ENV{GIT_AUTHOR_NAME} "${output}")
-    endif()
-
-    RunGitCommand(show -s "--format=%ae" ${object})
-    if(exit_code EQUAL 0)
-        set(ENV{GIT_AUTHOR_EMAIL} "${output}")
-    endif()
-
-    RunGitCommand(show -s "--format=%cI" ${object})
-    if(exit_code EQUAL 0)
-        set(ENV{GIT_COMMIT_DATE_ISO8601} "${output}")
-    endif()
-
-    RunGitCommand(show -s "--format=%s" ${object})
-    if(exit_code EQUAL 0)
-        set(ENV{GIT_COMMIT_SUBJECT} "${output}")
-    endif()
-
-    RunGitCommand(show -s "--format=%b" ${object})
-    if(exit_code EQUAL 0)
-        set(ENV{GIT_COMMIT_BODY} "${output}")
-    endif()
-
-    # >>>
-    # 2. Additional git properties can be added here via the
-    #    "execute_process()" command. Be sure to set them in
-    #    the environment using the same variable name you added
-    #    to the "_state_variable_names" list.
-
-endfunction()
-
-
-
-# Function: GitStateChangedAction
-# Description: this function is executed when the state of the git
-#              repository changes (e.g. a commit is made).
-function(GitStateChangedAction)
-    foreach(var_name ${_state_variable_names})
-        set(${var_name} $ENV{${var_name}})
+    # CMake has a seriously limited regex implementation, so use a small
+    # Perl script to parse the output of "git describe --always --dirty":
+    execute_process(
+        COMMAND "${CMAKE_CURRENT_LIST_DIR}/parse_tag_version.pl"
+        WORKING_DIRECTORY "${_working_dir}"
+        OUTPUT_VARIABLE output
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    # CMake also lacks an elegant way to split a line of key=value pairs into
+    # values, so this is the best way transform the command's output into the
+    # corresponding values for now:
+    string(REPLACE "\n" ";" lines ${output})
+    foreach(line IN LISTS lines)
+        string(REGEX MATCH "^[^=]+" key ${line})
+        string(REPLACE "${key}=" "" value ${line})
+        set(ENV{${key}} "${value}")
     endforeach()
-    configure_file("${PRE_CONFIGURE_FILE}" "${POST_CONFIGURE_FILE}" @ONLY)
 endfunction()
-
-
-
-# Function: HashGitState
-# Description: loop through the git state variables and compute a unique hash.
-# Args:
-#   _state (out)  string; a hash computed from the current git state.
-function(HashGitState _state)
-    set(ans "")
-    foreach(var_name ${_state_variable_names})
-        string(SHA256 ans "${ans}$ENV{${var_name}}")
-    endforeach()
-    set(${_state} ${ans} PARENT_SCOPE)
-endfunction()
-
-
-
-# Function: CheckGit
-# Description: check if the git repo has changed. If so, update the state file.
-# Args:
-#   _working_dir    (in)  string; the directory from which git commands will be ran.
-#   _state_changed (out)    bool; whether or no the state of the repo has changed.
-function(CheckGit _working_dir _state_changed)
-
-    # Get the current state of the repo.
-    GetGitState("${_working_dir}")
-
-    # Convert that state into a hash that we can compare against
-    # the hash stored on-disk.
-    HashGitState(state)
-
-    # Check if the state has changed compared to the backup on disk.
-    if(EXISTS "${GIT_STATE_FILE}")
-        file(READ "${GIT_STATE_FILE}" OLD_HEAD_CONTENTS)
-        if(OLD_HEAD_CONTENTS STREQUAL "${state}")
-            # State didn't change.
-            set(${_state_changed} "false" PARENT_SCOPE)
-            return()
-        endif()
-    endif()
-
-    # The state has changed.
-    # We need to update the state file on disk.
-    # Future builds will compare their state to this file.
-    file(WRITE "${GIT_STATE_FILE}" "${state}")
-    set(${_state_changed} "true" PARENT_SCOPE)
-endfunction()
-
-
 
 # Function: SetupGitMonitoring
 # Description: this function sets up custom commands that make the build system
@@ -242,10 +127,8 @@ function(SetupGitMonitoring)
     add_custom_target(check_git
         ALL
         DEPENDS ${PRE_CONFIGURE_FILE}
-        BYPRODUCTS
-            ${POST_CONFIGURE_FILE}
-            ${GIT_STATE_FILE}
-        COMMENT "Checking the git repository for changes..."
+        BYPRODUCTS ${POST_CONFIGURE_FILE}
+        COMMENT "Checking the git repository for changes"
         COMMAND
             ${CMAKE_COMMAND}
             -D_BUILD_TIME_CHECK_GIT=TRUE
@@ -257,24 +140,20 @@ function(SetupGitMonitoring)
             -P "${CMAKE_CURRENT_LIST_FILE}")
 endfunction()
 
-
-
 # Function: Main
 # Description: primary entry-point to the script. Functions are selected based
 #              on whether it's configure or build time.
 function(Main)
     if(_BUILD_TIME_CHECK_GIT)
-        # Check if the repo has changed.
-        # If so, run the change action.
-        CheckGit("${GIT_WORKING_DIR}" changed)
-        if(changed OR NOT EXISTS "${POST_CONFIGURE_FILE}")
-            GitStateChangedAction()
-        endif()
+        GetGitState("${GIT_WORKING_DIR}")
+        foreach(var_name ${_state_variable_names})
+            set(${var_name} $ENV{${var_name}})
+        endforeach()
+        configure_file("${PRE_CONFIGURE_FILE}" "${POST_CONFIGURE_FILE}" @ONLY)
     else()
         # >> Executes at configure time.
         SetupGitMonitoring()
     endif()
 endfunction()
 
-# And off we go...
 Main()
---
+++ parse_tag_version.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+#
+# Part of https://github.com/misje/cmake-git-version-tracking
+#
+# If a project uses git tags and semantic versioning, tags can be used to
+# automatically set the version number in the project. The following regex
+# parses the output of "git describe". Example tags:
+#
+# v1.2.3
+# v1.2
+# 1.2.3
+# 1.2.3-4
+# 2f7c290
+# v2.4.0~rc1-3
+# v2.4.0~rc1-3-104-gffba103
+#
+# The various parts of the version string are captures and printed as
+# key–value pairs. Missing parts are not printed unless they are integers, in
+# which case they are set to -1.
+
+use warnings;
+use strict;
+use utf8;
+    
+chomp(my $gitDescription = qx(git describe --always --dirty));
+$gitDescription =~ /
+\A
+v?                                     # Optional "version v"
+(?<full_extra>                         # Capture everything before deb revision
+(?<full>                               # ↑, except the "extra part
+(?<major>\d+)                          # Major
+\.                                     # Period separator
+(?<minor>\d+)                          # Minor
+(?:\.(?<patch>\d+))?                   # Optional period separator and patch
+)
+(?<extra>[^-]+)?                       # Any text other than "-"
+)
+(?:-(?<revision>\d+)(?!-g[0-9af]+))?   # Optional Debian revision
+(?:-(?<commits>\d+)                    # Optional commit count
+-g(?<sha>[0-9a-f]+))?                  # Optinal git SHA hash
+|                                      # … or
+(?<sha>[0-9a-f]+)                      # Just the git SHA hash
+/x;
+
+# Print matched group by name, uppercase, prefixed by GIT_TAG_VERSION_:
+print "GIT_TAG_VERSION_", uc $_, "=$+{$_}\n" for keys %+;
+# If major and minor, print "major.minor(.patch)", otherwise sha:
+print "GIT_TAG_VERSION_ANY=", (exists $+{'sha'} ? $+{'sha'} : $+{'full'}), "\n";
+# Assign -1 to integer variables that were not matched:
+foreach (qw(major minor patch revision commits)) {
+    print "GIT_TAG_VERSION_", uc $_, "=-1\n" if not exists $+{$_};
+}
+# Create a binary variable for dirtyness:
+print "GIT_TAG_VERSION_DIRTY=", $gitDescription =~ /-dirty\Z/ ? 1 : 0, "\n";
